70371f4b579d163e7d18041c3c20a44c6cb4b8a6
KaikeRochaDev <kaikerochadev@gmail.com>
2022-11-11 17:42:35 -0300

    create request test

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_View(request):
+    return HttpResponse('Uma linda string')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_View)
 ]
