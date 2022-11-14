df485ff692bc9c16c80715a6dca59cd7b8943bf7
KaikeRochaDev <kaikerochadev@gmail.com>
2022-11-14 14:15:55 -0300

    setting URL parameters

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Kaike Rocha',
     })
+    
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Kaike Rocha',
+    })
+
